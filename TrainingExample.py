d156aac8c6fc097a78133ce198d2ad10ab478b4c
jonaswehding <32729836+jonaswehding@users.noreply.github.com>
2025-03-25 09:30:03 +0100

    Update TrainingExample.py
    Update to YOLO11

--- TrainingExample.py
+++ TrainingExample.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov11n.pt')  # load a pretrained model (recommended for training)
 
 # Train the model with 2 GPUs
 model.train(data='datasets/frugt.yaml', epochs=1000, imgsz=640, device=0)
